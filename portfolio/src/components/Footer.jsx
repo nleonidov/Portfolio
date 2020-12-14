cf67facb3a4ab7654eb45fcdd568ede180471aea
Nick Leonidov <nickleonidov@Nicks-MacBook-Air.local>
2020-12-14 14:04:46 -0500

    Updated footer/about

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -10,13 +10,14 @@ export default function Footer() {
                 <a className='email-link' href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZXhczSBXdkjVjbsbbsGHKprRqgTrWdHfNfJsfCSNSFWDdzHBLzVrbpZwfwVQzKKdPtVNB">
                     <i class="gg-mail">Email</i>
                 </a>
-            <br />
                 <a className='linkedin' href="https://www.linkedin.com/in/nickleonidov/">
                     <i class="gg-mail">LinkedIn</i>
                 </a>
-            <br />
                 <a className='github' href="https://github.com/nleonidov/">
                     <i class="gg-mail">Github</i>
+                </a>
+                <a className='soundcloud' href="https://soundcloud.com/leonicbass">
+                    <i class="gg-sc">SoundCloud</i>
                 </a>
             </div>
         </div>
